2703575e8b66ea40de919049554b386bf03e1b61
root <jorgefernandoochoa1@hotmail.com>
2023-06-07 11:11:23 -0700

    Replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,5 +2,5 @@
 def new_in_list(my_list, idx, element):
     cplist = my_list[:]
     if idx >= 0 and idx < len(my_list):
-        cplist[idx] = elemnt
+        cplist[idx] = element
     return cplist
