8efcc62690685a4ca40c338f724c71015782adee
root <jorgefernandoochoa1@hotmail.com>
2023-06-07 11:22:43 -0700

    Remove the c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new = ""
     for i in my_string:
-        if my_string[i] == 'c' or my_string == 'C':
-            my_string[i] = ""
-    return my_string
+        if my_string[i] != 'c' or my_string != 'C':
+            new += i
+    return new
+
