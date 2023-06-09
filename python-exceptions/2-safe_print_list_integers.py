353036bb3827986bfe3c48c675d57941c0c1c63c
root <jorgefernandoochoa1@hotmail.com>
2023-06-09 11:37:39 -0700

    P & Count integers

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    con = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            con += 1
+        except (ValueError, ValueError):
+            pass
+        print()
+        return con
