03bd09291d90b946d9719085c3716ce144fb1c7a
root <jorgefernandoochoa1@hotmail.com>
2023-06-13 09:02:40 -0700

    square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """reptresent a square"""
+
+    def __init__(self, size):
+        """initiates size value"""
+
+        self.__size = size
