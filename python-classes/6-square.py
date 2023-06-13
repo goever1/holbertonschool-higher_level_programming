ea672d274ff1f17e6b6918a351a009e984e5dc5b
root <jorgefernandoochoa1@hotmail.com>
2023-06-13 10:40:47 -0700

    coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Defines square"""
 
+
 class Square:
     """defines square with size"""
+
     def __init__(self, size=0, position=(0, 0)):
         if type(size) != int:
             raise TypeError("size must be an integer")
@@ -11,7 +13,8 @@ class Square:
         if (type(position) == tuple and len(position) == 2):
             for value in position:
                 if type(value) != int or value < 0:
-                    raise TypeError("position must be a tuple fo 2 positive integers")
+                    raise TypeError(
+                            "position must be a tuple fo 2 positive integers")
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
 
@@ -32,6 +35,10 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
     
+    def area(self):
+        """return the area of the square"""
+        return self.__size**2
+    
     @property
     def position(self):
         return self.__position
@@ -47,17 +54,13 @@ class Square:
 
         self.__position = value
 
-    def area(self):
-        """Return the area of the square"""
-        return self.__size**2
-
     def my_print(self):
         if self.size > 0 and self.position[1] > 0:
             for i in range(self.position[1]):
                 print()
         if self.size != 0:
             for i in range(self.size):
-                for i in range(self.size[0]):
+                for i in range(self.position[0]):
                     print(" ", end="")
                 for i in range(self.size):
                     print("#", end="")
