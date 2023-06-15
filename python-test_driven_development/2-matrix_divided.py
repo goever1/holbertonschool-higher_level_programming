5ebaec9f88d409b789a6278ca8050ac2d4a7bfd2
root <jorgefernandoochoa1@hotmail.com>
2023-06-15 11:58:59 -0700

    matrix divides

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""divides all elements of a matrix"""
+
+
+def matrix_divided(matrix, div):
+    """ divides all elemnts of a matrix"""
+    
+    if len(set(map(len, matrix))) != 1:
+        raise TypeError("Each row of the matrix must have the same size")
+    for array in matrix:
+            for elemnt in array:
+                if type(elemnt) not in [int, float]
+                raise TypeError(
+                        "matrix must be a matrix (list of lists)"
+                        " of integers/floats")
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
