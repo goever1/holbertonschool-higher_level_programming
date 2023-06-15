c629cea5734e378e72967c124633108d09517bb7
goever1 <30449167+goever1@users.noreply.github.com>
2023-06-15 12:25:51 -0500

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" function that add two integer"""
+
+
 def add_integer(a, b=98):
+    """ a + b """
+    
     if (type(a) != int and (type(a) != float)):
         raise TypeError("a must be an integer")
     if (type(b) != int and (type(b) != float)):
