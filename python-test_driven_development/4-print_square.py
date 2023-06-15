fae8466b1a40ef847e87a0866e63663dcfb6aa6a
goever1 <30449167+goever1@users.noreply.github.com>
2023-06-15 15:04:04 -0500

    Update 4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,10 +4,12 @@
 def print_square(size):
     """ function that prints a square"""
 
-    if not isinstance(size, int) and size < 0:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    if size == float and size < 0:
+        raise TypeError("size must be an integer")
 
     for i in range(size):
         print("#" * size)
