ebc59fd90ffac24a47f3aac4e6076f2e00200064
goever1 <30449167+goever1@users.noreply.github.com>
2023-07-26 13:56:22 -0500

    groups

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- It lists the number of records with the same score in the table second_table of the database hbtn_0c_0
+SELECT score, COUNT(score) AS number
+FROM second_table
+GROUP BY scroe
+ORDER BY number DESC;
