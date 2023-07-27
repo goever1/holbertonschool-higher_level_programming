95a5b4889b8e712c7ad0b1797e27be7275a0b7eb
goever1 <30449167+goever1@users.noreply.github.com>
2023-07-27 11:45:57 -0500

    states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- It creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name VARCHAR(256) NOT NULL);
