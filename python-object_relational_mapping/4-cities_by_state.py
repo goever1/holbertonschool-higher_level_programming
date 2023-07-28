05232c750fee279ad1f75cf2823725c1c431fc97
goever1 <30449167+goever1@users.noreply.github.com>
2023-07-28 15:22:39 -0500

    cities by state

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
   import sys
   import MySQLdb
   
-  db = (host='localhost', port=3306, user=sys.argv[1],
+  db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
         passwd=sys.argv[2], db=sys.argv[3])
   cur = db.cursor()
   cur.execute("SELECT cities.id, cities.name, states.name\
