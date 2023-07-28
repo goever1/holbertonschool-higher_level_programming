f70e082952143e2e5254e8ee35bffb8f0a06a862
goever1 <30449167+goever1@users.noreply.github.com>
2023-07-28 16:29:21 -0500

    Update 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,20 +3,18 @@
 It lists all State objects from the database hbtn_0e_6_usa
 '''
 
-if __name__ == '__main__':
-  import sys
-  from model_state import Base, State
-  from sqlalchemy.orm import sessionmaker
-  
-  from sqlalchemy import (create_engine)
+from sys import argv
+from sqlalchemy import create_engine
+import sqlalchemy as db
+from model_state import Base, State
 
-  engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                         pool_pre_ping=True)
-  Base.metadata.create_all(engine)
-  Session = sessionmaker(bind=engine)
-  session = Session()
-  for item in session.query(State).order_by(State.id):
-    print("{}: {}".format(item.id, item.name))
-    
-  session.close()
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3]))
+
+    connection = engine.connect()
+    states = db.select(State.id, State.name).order_by(State.id)
+    result = connection.execute(states).fetchall()
+
+    for state in result:
+        print("{}: {}".format(state[0], state[1]))
