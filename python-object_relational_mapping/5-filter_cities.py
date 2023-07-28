3efd4054f3437d12cd88bfd2cff495943e0215de
goever1 <30449167+goever1@users.noreply.github.com>
2023-07-28 16:03:50 -0500

    filter cities

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
   db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], 
                        passwd=sys.argv[2], db=sys.argv[3])
   cur = db.cursor()
-  db.execute("SELECT * FROM `cities` as `c` \
+  cur.execute("SELECT * FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
