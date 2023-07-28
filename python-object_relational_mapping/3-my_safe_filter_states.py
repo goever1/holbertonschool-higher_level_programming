dca2528b78e5b592bdd742b0b913fed08c01726a
goever1 <30449167+goever1@users.noreply.github.com>
2023-07-28 14:36:10 -0500

    my safe filter states

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,8 +10,9 @@ it write a script that takes in arguments and displays all values in the states 
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    to = "SELECT * FROM states WHERE BINARY name='{}' ORDER BY id ASC"
-    cur.execute(to, (sys.argv[4]))
+    st = sus.argv[4]
+    to = "SELECT * FROM states WHERE BINARY name= %s ORDER BY id ASC"
+    cur.execute(to, st)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
