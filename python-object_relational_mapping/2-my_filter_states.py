0189ca95b22583839be730b55636488dad26a4a6
goever1 <30449167+goever1@users.noreply.github.com>
2023-07-28 14:01:49 -0500

    my filter states

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+it takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+'''
+
+if __name__ = __main__:
+  import sys
+  import MySQLdb
+
+  db = MySQL.connect( host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sus.argv[3])
+  cur = db.cursor()
+  st = sys.argv[4]
+  to = "SELECT * FROM states WHERE BINARY name='{}'\ ORDER BY id ASC".format(st)
+  cur.execute(to)
+  states = cur.fetchall()
+  for state in states:
+    print(state)
+  cur.close()
