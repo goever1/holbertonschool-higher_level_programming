50055313142cb57d8b06d2c3f968e504b875123d
goever1 <30449167+goever1@users.noreply.github.com>
2023-09-14 09:43:08 -0500

    Create 1-script.js

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+const tag = document.getElementById('red_header');
+const header = document.querySelector('header');
+tag.addEventListener('click', function () {
+  header.style.color = '#FF0000';
+});
